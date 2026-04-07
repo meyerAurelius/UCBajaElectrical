04bcea88a7c9a348f3661fb8e76a35ee25a8a584
emmamaiko717 <emmamaiko717@gmail.com>
2026-04-06 22:24:18 -0600

    UART example implementation

--- BAJA/digital_dash/front_end/esp_based/main/controller.c
+++ BAJA/digital_dash/front_end/esp_based/main/controller.c
@@ -1,9 +1,116 @@
-// code for digital dash controller
+// code for digital dash controller UART
 
-// interpreting UART readout
+#include "controller.h"
+
+// from async_rxtxtasks example
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (CONFIG_EXAMPLE_UART_TXD)           //set in menuconfig
+#define RXD_PIN (CONFIG_EXAMPLE_UART_RXD)           //GPIO27
+
+void init(void) //driver install, communication parameters and pins
+{
+    const uart_config_t uart_config = {
+        .baud_rate = CONFIG_EXAMPLE_UART_BAUD_RATE,         //9600
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
 
 
-// functions
+static void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            
+            //button logic
+
+            uint8_t button = data[0];
+                
+            switch(button) {
+
+                 case 1: 
+                    move_down();
+                    break;
+
+                case 2:
+                    select();
+                    break;
+
+                case 3:
+                    move_right();
+                    break;
+
+                case 4:
+                    move_left();
+                    break;
+
+                case 5:
+                    move_up();
+                    break;
+
+                default:
+                    printf("Unidentified data: %d\n", button);                  //if controller sends something not recognized
+                    break;
+            }
+
+
+
+        }
+    }
+    free(data);                     //deallocates dynamically allocated memory
+}
+
+
+void app_main(void)                 
+{
+    init();                         //call communication parameters
+    xTaskCreate(rx_task, "uart_rx_task", CONFIG_EXAMPLE_TASK_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, NULL);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- BAJA/digital_dash/front_end/esp_based/main/controller.h
+++ BAJA/digital_dash/front_end/esp_based/main/controller.h
@@ -1,3 +1,39 @@
 #pragma once
+#include <stdio.h>
+
+//dash functions
+
+void move_left(void) {
+
+    printf("Left\n");
+}
+    
+    
+
+void move_right(void) {
+
+     printf("Right\n");
+}
+   
 
 
+void move_up(void) {
+
+    printf("Up\n");
+}
+    
+
+
+
+void move_down(void) {
+
+    printf("Down\n");
+}
+   
+
+
+void select_button(void) {
+
+    printf("Select\n");
+}
+    
