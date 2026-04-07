14ea3dc9e925795e7aee02bbb1a7c3bd1fc71213
Marcus Meyer <mm7472889@gmail.com>
2026-04-06 22:12:59 -0600

    digital dash gets temperature valye via esp now and displays on the dash. Basic slider with bounds, large text temperature label, and an engine temp icon.

--- BAJA/digital_dash/front_end/esp_based/main/demo.c
+++ BAJA/digital_dash/front_end/esp_based/main/demo.c
@@ -26,7 +26,14 @@
 #include "esp_now.h"
 
 #include "espnow_example.h"
+
+
+// image declaration
+LV_IMAGE_DECLARE(engine_temp_light);
+
 static lv_obj_t *lbl_counter = NULL;
+static lv_obj_t *temp_label = NULL;
+static lv_obj_t * temp_slide = NULL;
 
 // thermistor esp mac 94:A9:90:0B:2A:04
 static uint8_t s_peer_mac[6] =  { 0x94, 0xA9, 0x90, 0x0B, 0x2A, 0x04 };
@@ -58,8 +65,7 @@ static void ui_event_button_1(lv_event_t *e)
     }
 }
 
-int cvt_temp = 30;  
-int vehicle_speed = 11;
+
 
 static esp_err_t app_lvgl_main(void)
 {
@@ -84,21 +90,52 @@ static esp_err_t app_lvgl_main(void)
 	lv_obj_set_x(button_1, 228);
     lv_obj_set_y(button_1, 5);
     
-    lv_obj_t * temp_slide = lv_slider_create(lv_obj_0);
+    temp_slide = lv_slider_create(lv_obj_0);
 
 	lv_obj_set_x(temp_slide, 7);
     lv_obj_set_y(temp_slide, 190);
     lv_obj_set_width(temp_slide, 131);
     lv_obj_set_height(temp_slide, 18);
     lv_obj_set_style_transform_rotation(temp_slide, -900, 0);
+    lv_slider_set_range(temp_slide, 0, 125);
     
-    lv_obj_t * h4_1 = lv_label_create(lv_obj_0);
-    lv_label_set_text(h4_1, "CVT   Temperature");
-    lv_obj_set_style_text_color(h4_1, lv_color_black(), LV_PART_MAIN);
+    // adjust knob size
+    lv_obj_set_style_pad_left(temp_slide, -10, LV_PART_KNOB);
+    lv_obj_set_style_pad_right(temp_slide, -10, LV_PART_KNOB);
+    lv_obj_set_style_pad_top(temp_slide, -10, LV_PART_KNOB);
+    lv_obj_set_style_pad_bottom(temp_slide, -10, LV_PART_KNOB);
 
-	lv_obj_set_x(h4_1, 10);
-    lv_obj_set_y(h4_1, 210);
+    // temp icon
+    lv_obj_t *temp_icon = lv_image_create(lv_obj_0);
+    lv_image_set_src(temp_icon, &engine_temp_light);
+    lv_obj_set_x(temp_icon, 40);
+    lv_obj_set_y(temp_icon, 120);
+
+    // lv_obj_t * h4_1 = lv_label_create(lv_obj_0);
+    // lv_label_set_text(h4_1, "CVT   Temperature");
+    // lv_obj_set_style_text_color(h4_1, lv_color_black(), LV_PART_MAIN);
+
+	// lv_obj_set_x(h4_1, 10);
+    // lv_obj_set_y(h4_1, 210);
+
+    lv_color_t orange_red = lv_color_make(245, 84, 66);
+
     
+    // upper bound limit indicator on slider
+    lv_obj_t *max_temp_label = lv_label_create(lv_obj_0);
+    lv_label_set_text(max_temp_label, "125°C");
+    lv_obj_set_style_text_color(max_temp_label, orange_red, LV_PART_MAIN);
+    lv_obj_set_x(max_temp_label, 7);
+    lv_obj_set_y(max_temp_label, 35);
+
+    // lower bound
+    lv_obj_t *min_temp_label = lv_label_create(lv_obj_0);
+    lv_label_set_text(min_temp_label, "0°C");
+    lv_obj_set_style_text_color(min_temp_label, orange_red, LV_PART_MAIN);
+    lv_obj_set_x(min_temp_label, 7);
+    lv_obj_set_y(min_temp_label, 205);
+
+
     lv_obj_t * arc_1 = lv_arc_create(lv_obj_0);
 
 	lv_obj_set_x(arc_1, 144);
@@ -113,14 +150,18 @@ static esp_err_t app_lvgl_main(void)
 	lv_obj_set_x(h3_1, 200);
     lv_obj_set_y(h3_1, 210);
     
-    lv_obj_t * temp_value = lv_label_create(lv_obj_0);
-    lv_label_set_text(temp_value, "temp val");
-    lv_obj_set_style_text_color(temp_value, lv_color_black(), LV_PART_MAIN);
+    temp_label = lv_label_create(lv_obj_0);
+    char temp_val[12];
+    snprintf(temp_val, sizeof(temp_val), "%.2f", recv_temp[0]);
+    strcat(temp_val, " °C");
+    lv_label_set_text(temp_label, temp_val);
+    lv_obj_set_style_text_color(temp_label, lv_color_black(), LV_PART_MAIN);
+    lv_obj_set_style_text_font(temp_label, &lv_font_montserrat_30, 0);
 
-	lv_obj_set_x(temp_value, 38);
-    lv_obj_set_y(temp_value, 160);
-    lv_obj_set_width(temp_value, 79);
-    lv_obj_set_height(temp_value, 48);
+	lv_obj_set_x(temp_label, 38);
+    lv_obj_set_y(temp_label, 80);
+    lv_obj_set_width(temp_label, 130);
+    lv_obj_set_height(temp_label, 48);
 
     
     
@@ -161,6 +202,72 @@ static void comms_task(void)
     ESP_ERROR_CHECK(example_espnow_init());
 }
 
+static void gui_task(void* ){
+        
+        esp_lcd_panel_io_handle_t lcd_io = NULL;
+        esp_lcd_panel_handle_t lcd_panel = NULL;
+        esp_lcd_touch_handle_t tp = NULL;
+        lvgl_port_touch_cfg_t touch_cfg = {0};
+        lv_display_t *lvgl_display = NULL;
+
+        char buf[16];
+        uint16_t n = 0;
+
+        ESP_ERROR_CHECK(lcd_display_brightness_init());
+
+        ESP_ERROR_CHECK(app_lcd_init(&lcd_io, &lcd_panel));
+
+        lvgl_display = app_lvgl_init(lcd_io, lcd_panel);
+        if (lvgl_display == NULL) {
+            ESP_LOGE(TAG, "Fatal error in app_lvgl_init");
+            esp_restart();
+        }
+
+        ESP_ERROR_CHECK(touch_init(&tp));
+
+        touch_cfg.disp = lvgl_display;
+        touch_cfg.handle = tp;
+        lvgl_port_add_touch(&touch_cfg);
+
+        ESP_ERROR_CHECK(lcd_display_brightness_set(100));
+        ESP_ERROR_CHECK(lcd_display_rotate(lvgl_display, LV_DISPLAY_ROTATION_90));
+
+        ESP_ERROR_CHECK(app_lvgl_main());
+
+
+        while (1) {
+       
+        snprintf(buf, sizeof(buf), "%04u", n++);
+
+        if (lvgl_port_lock(0)) {
+            // code related to update must be inside here to be threadsafe
+            // watchdog timer triggers otherwise 
+
+            if (lbl_counter != NULL) {
+                lv_label_set_text(lbl_counter, buf);
+            }
+            
+            // accessing recv_temp should be accessed in a threadsafe way (todo)
+            if (recv_temp[0] == -1.0){
+                lv_label_set_text(temp_label, "NOT CONNECTED!");
+                lv_obj_set_style_text_font(temp_label, &lv_font_montserrat_14, 0);
+            } else{
+                char temp_val[12];
+                snprintf(temp_val, sizeof(temp_val), "%d", (int)recv_temp[0]);
+                strcat(temp_val, " °C");
+                lv_label_set_text(temp_label, temp_val);
+                lv_obj_set_style_text_font(temp_label, &lv_font_montserrat_30, 0);
+
+                lv_slider_set_value(temp_slide, (int)recv_temp[0], LV_ANIM_ON);
+            }
+        
+
+            lvgl_port_unlock();
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(125));
+    }
+}
 
 
 void app_main(void)
@@ -188,51 +295,12 @@ void app_main(void)
 
 
 
-    esp_lcd_panel_io_handle_t lcd_io = NULL;
-    esp_lcd_panel_handle_t lcd_panel = NULL;
-    esp_lcd_touch_handle_t tp = NULL;
-    lvgl_port_touch_cfg_t touch_cfg = {0};
-    lv_display_t *lvgl_display = NULL;
-
-    char buf[16];
-    uint16_t n = 0;
-
-    ESP_ERROR_CHECK(lcd_display_brightness_init());
-
-    ESP_ERROR_CHECK(app_lcd_init(&lcd_io, &lcd_panel));
-
-    lvgl_display = app_lvgl_init(lcd_io, lcd_panel);
-    if (lvgl_display == NULL) {
-        ESP_LOGE(TAG, "Fatal error in app_lvgl_init");
-        esp_restart();
-    }
-
-    ESP_ERROR_CHECK(touch_init(&tp));
-
-    touch_cfg.disp = lvgl_display;
-    touch_cfg.handle = tp;
-    lvgl_port_add_touch(&touch_cfg);
-
-    ESP_ERROR_CHECK(lcd_display_brightness_set(100));
-    ESP_ERROR_CHECK(lcd_display_rotate(lvgl_display, LV_DISPLAY_ROTATION_90));
-
-    ESP_ERROR_CHECK(app_lvgl_main());
-
 
     
     comms_task();
 
-    while (1) {
+    xTaskCreatePinnedToCore(gui_task, "gui_task", 16384, NULL, 5, NULL, 1);
 
-        snprintf(buf, sizeof(buf), "%04u", n++);
 
-        if (lvgl_port_lock(0)) {
-            if (lbl_counter != NULL) {
-                lv_label_set_text(lbl_counter, buf);
-            }
-            lvgl_port_unlock();
-        }
 
-        vTaskDelay(pdMS_TO_TICKS(125));
-    }
 }
