1c82e8ed00e0a990338cf2600e5ece1c97f3dd87
emmamaiko717 <emmamaiko717@gmail.com>
2026-04-08 19:54:28 -0600

    added UART kconfig

--- BAJA/digital_dash/front_end/esp_based/main/controller.h
+++ BAJA/digital_dash/front_end/esp_based/main/controller.h
@@ -1,39 +1,9 @@
 #pragma once
 #include <stdio.h>
 
-//dash functions
 
-void move_left(void) {
-
-    printf("Left\n");
-}
-    
-    
-
-void move_right(void) {
-
-     printf("Right\n");
-}
-   
-
-
-void move_up(void) {
-
-    printf("Up\n");
-}
-    
-
-
-
-void move_down(void) {
-
-    printf("Down\n");
-}
-   
-
-
-void select_button(void) {
-
-    printf("Select\n");
-}
-    
+void move_down(void);
+void move_up(void);
+void move_right(void);
+void move_left(void);
+void select(void);
